d9a1aa9553ffc2c1b3114a50594a040c10c337a5
zynx <52384539+gmZynx@users.noreply.github.com>
2023-08-08 21:29:41 +0300

    same hacky missile fix for APC

--- lua/simfphys_weapons/combineapc.lua
+++ lua/simfphys_weapons/combineapc.lua
@@ -134,9 +134,18 @@ function simfphys.weapon:Think( vehicle )
 					vehicle.missle:SetPos( attch.Pos )
 					vehicle.missle:SetAngles( attch.Ang - Angle(15,0,0) )
 					vehicle.missle:SetOwner( vehicle )
-					vehicle.missle:SetSaveValue( "m_flDamage", 250 )
+					vehicle.missle:SetSaveValue( "m_flDamage", 0 )
 					vehicle.missle:Spawn()
 					vehicle.missle:Activate()
+
+					local missile = vehicle.missle
+					local future_owner = vehicle:GetDriver()
+					timer.Simple( .7, function()
+						if missile:IsValid() then
+							missile:SetOwner( future_owner )
+							missile:SetSaveValue( "m_flDamage", 120 )
+						end
+					end )
 					
 					vehicle.missle.DirVector = vehicle.missle:GetAngles():Forward()
 					
